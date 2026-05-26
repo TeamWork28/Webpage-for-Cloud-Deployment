f6209f2144da2aea424a4f8936c2f1717c40f526
the_royalty <shravansen.sk@gmail.com>
2026-05-26 17:43:11 +0530

    updated the app.py file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, render_template, redirect, url_for, session
+from src.pipeline.predict_pipeline import PredictPipeline, CustomData
 
 app = Flask(
     __name__,
