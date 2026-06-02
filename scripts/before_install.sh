6247664820c2f1debfd90815e6a8c5b974f5b1ce
the_royalty <shravansen.sk@gmail.com>
2026-06-02 11:34:16 +0530

    CodeDeploy fixes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
 # Create destination directory if it doesn't exist
 mkdir -p /app/student-performance
+
+# Fix potential package manager locks
+dpkg --configure -a
 
 # Install required packages
 apt-get update -y
 apt-get install -y docker.io awscli
 
-# Start docker
+# Start Docker
 systemctl start docker
 systemctl enable docker
